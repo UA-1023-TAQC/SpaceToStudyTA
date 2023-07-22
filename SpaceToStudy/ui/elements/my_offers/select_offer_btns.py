5aae66be71bc0a8ce47e50731225b050df5a9888
Anastasiia <maschenkonastya@gmail.com>
2023-07-22 21:30:10 +0300

    Add My Offers page

---
+++ SpaceToStudy/ui/elements/my_offers/select_offer_btns.py
@@ -0,0 +1,34 @@
+from selenium.webdriver.common.by import By
+
+ALL_BTN = (By.XPATH, "./button[1]")
+ACTIVE_BTN = (By.XPATH, "./button[2]")
+DRAFT_BTN = (By.XPATH, "./button[3]")
+CLOSED_BTN = (By.XPATH, "./button[4]")
+
+class SelectOffers:
+    def __init__(self, noda):
+        self.noda = noda
+
+    def get_all_btn(self):
+        return self.noda.find_element(*ALL_BTN)
+
+    def get_active_btn(self):
+        return self.noda.find_element(*ACTIVE_BTN)
+
+    def get_draft_btn(self):
+        return self.noda.find_element(*DRAFT_BTN)
+
+    def get_closed_btn(self):
+        return self.noda.find_element(*CLOSED_BTN)
+
+    def click_all_btn(self):
+        self.get_all_btn().click()
+
+    def click_active_btn(self):
+        self.get_active_btn().click()
+
+    def click_draft_btn(self):
+        self.get_draft_btn().click()
+
+    def click_closed_btn(self):
+        self.get_closed_btn().click()
