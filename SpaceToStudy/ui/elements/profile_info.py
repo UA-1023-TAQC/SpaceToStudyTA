0cba0452f3aaa99cdef49ab173f64988ec230caa
Volodumur Besarab <volodumur911@gmail.com>
2023-07-27 22:24:17 +0300

    refactor profile_info

--- SpaceToStudy/ui/elements/profile_info.py
+++ SpaceToStudy/ui/elements/profile_info.py
@@ -3,35 +3,23 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
-#/html/body/div/div/div[2]/div[2]
-#/html/body/div/div/div[2]/div[2]/div[1]/a
-
 AVATAR = (By.XPATH, "./div[1]/div[1]/div")
-
 NAME_SURNAME = (By.XPATH, "./div[1]/div[2]/div[1]/p")
 DAYS_IN_SPASE_TO_STUDY = (By.XPATH, "./div[1]/div[2]/div[3]/div[1]/p")
 TEXT_UNDER_DAYS_IN_SPASE_TO_STUDY = (By.XPATH, "./div[1]/div[2]/div[3]/div[1]/span")
-
-PROFILE_RATING = (By.XPATH, "./div[1]/div[2]/div[3]/div[2]/span/div/div/p")
-PROFILE_RATING_ICON = (By.XPATH, "./div[1]/div[2]/div[3]/div[2]/span/div/div/svg/path") #does not work
 REVIEW_COUNTER = (By.XPATH, "./div[1]/div[2]/div[3]/div[2]/span/div/a")
-
-NATIVE_LANGUAGE = (By.XPATH, "./div[1]/div[2]/div[4]/div/div[1]/div[1]")
-NATIVE_LANGUAGE_ICON = (By.XPATH, "./div[1]/div[2]/div[4]/div/div[1]/div[1]/div[2]/svg") #does not work
-
-CITY_COUNTRY_BASED = (By.XPATH, "./div[1]/div[2]/div[4]/div/div[1]/div[2]")
-CITY_COUNTRY_BASED_ICON = (By.XPATH, "./div[1]/div[2]/div[4]/div/div[1]/div[2]/div[1]/svg") #does not work
-
 EDIT_PROFILE_BUTTON = (By.XPATH, "./div[1]/a")
-EDIT_PROFILE_BUTTON_ICON = (By.XPATH, "./div[1]/a/svg/path") #does not work
 
+PROFILE_RATING_CONTAINER = (By.XPATH, "./div[1]/div[2]/div[3]/div[2]/span/div/div/p")
+NATIVE_LANGUAGE_CONTAINER = (By.XPATH, "./div[1]/div[2]/div[4]/div/div[1]/div[1]")
+CITY_COUNTRY_BASED_CONTAINER = (By.XPATH, "./div[1]/div[2]/div[4]/div/div[1]/div[2]")
 
 class ProfileInfo:
     def __init__(self, noda):
         self.noda = noda
 
-    def get_avatar(self) -> WebElement:
-        return self.noda.find_element(*AVATAR)
+    def get_avatar(self):
+        return self.noda.find_element(*AVATAR).find_element(By.XPATH, "//*[local-name()='svg']")
 
     def get_name_surname_text(self) -> str:
         return self.noda.find_element(*NAME_SURNAME).text
@@ -43,29 +31,29 @@ class ProfileInfo:
         return self.noda.find_element(*TEXT_UNDER_DAYS_IN_SPASE_TO_STUDY).text
 
     def get_native_language_text(self) -> str:
-        return self.noda.find_element(*NATIVE_LANGUAGE).text
+        return self.noda.find_element(*NATIVE_LANGUAGE_CONTAINER).text
 
     def get_native_language_icon(self) -> WebElement:
-        """DOES NOT WORK"""
-        return self.noda.find_element(*NATIVE_LANGUAGE).get_attribute("svg")
+        return self.noda.find_element(*NATIVE_LANGUAGE_CONTAINER).find_element(By.XPATH, "//*[local-name()='svg']")
 
     def get_profile_rating_text(self) -> str:
-        return self.noda.find_element(*PROFILE_RATING).text
+        return self.noda.find_element(*PROFILE_RATING_CONTAINER).text
 
     def get_profile_rating_icon(self) -> WebElement:
-        return self.noda.find_element(*PROFILE_RATING_ICON)
+        return self.noda.find_element(*PROFILE_RATING_CONTAINER).find_element(By.XPATH, "//*[local-name()='svg']")
 
     def get_review_counter_text(self) -> str:
         return self.noda.find_element(*REVIEW_COUNTER).text
 
-    def get_city_country_based(self) -> str:
-        return self.noda.find_element(*CITY_COUNTRY_BASED).text
+    def get_city_country_based_text(self) -> str:
+        return self.noda.find_element(*CITY_COUNTRY_BASED_CONTAINER).text
+
+    def get_city_country_based_icon(self) -> str:
+        return self.noda.find_element(*CITY_COUNTRY_BASED_CONTAINER).find_element(By.XPATH, "//*[local-name()='svg']")
 
     def click_edit_profile_button(self):
-        """DOES NOT WORK"""
-        edit_profile_button = self.noda.find_element(*CITY_COUNTRY_BASED)
+        edit_profile_button = self.noda.find_element(*EDIT_PROFILE_BUTTON)
         edit_profile_button.click()
-        time.sleep(2)
 
     def get_edit_profile_button(self) -> WebElement:
         return self.noda.find_element(*EDIT_PROFILE_BUTTON)
