0b8917eeac43c882f8b1aeb7e9176b50eb037134
Natali-natali <rozdilska.n@gmail.com>
2023-08-15 13:35:24 +0300

    fix search input

--- SpaceToStudy/ui/pages/home_page/home_student.py
+++ SpaceToStudy/ui/pages/home_page/home_student.py
@@ -1,19 +1,15 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
-from SpaceToStudy.ui.elements.home_page.categories import PopularCategories
-from SpaceToStudy.ui.elements.home_page.questions import AskedQuestions
 from SpaceToStudy.ui.pages.base_page import BasePage
 from SpaceToStudy.ui.pages.home_page.category_component import CategoryComponent
 from SpaceToStudy.ui.pages.home_page.search_tutor_input_component import SearchTutorComponent
 
-CATEGORIES_BTN = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[2]/div[2]/button")
-FIND_TUTOR_BTN = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[3]/a")
-QUESTIONS = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[4]/div[2]")
+
 CATEGORIES_BLOCK = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[2]/div[2]")
 CATEGORIES = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[2]/div[2]/div/a")
 
-SEARCH_INPUT_BLOCK = (By.XPATH, "/html/body/div/div/div[2]/div/div[1]/div/div[2]")
+SEARCH_INPUT_BLOCK = (By.XPATH, "/html/body/div/div/div[2]/div/div[1]/div")
 
 
 class HomePageStudent(BasePage):
@@ -33,31 +29,13 @@ class HomePageStudent(BasePage):
 
         return self._categories
 
+
     def get_search_input(self) -> WebElement:
         if not self._search_tutor:
             node = self.driver.find_element(*SEARCH_INPUT_BLOCK)
             self._search_tutor = SearchTutorComponent(node)
         return self._search_tutor
 
-    def get_categories_btn(self):
-        return self.driver.find_element(*CATEGORIES_BTN)
 
-    def click_categories_btn(self):
-        self.get_categories_btn().click()
 
-    def get_find_tutor_btn(self):
-        return self.driver.find_element(*FIND_TUTOR_BTN)
-
-    def click_find_tutor_btn(self):
-        self.get_find_tutor_btn().click()
-
-    def get_text_questions(self):
-        noda = self.driver.find_element(*QUESTIONS)
-        self._questions_block = AskedQuestions(noda)
-        return self._questions_block
-
-    def get_blocks_popular_categories(self):
-        noda = self.driver.find_element(*CATEGORIES_BLOCK)
-        self._categories_block = PopularCategories(noda)
-        return self._categories_block
 
