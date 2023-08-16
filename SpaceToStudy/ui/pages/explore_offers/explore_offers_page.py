91fa1524dfc02f1f6ea770cfe7233b63a4ce1eeb
Illia Nakonechnyi <92325403+Naccarato21@users.noreply.github.com>
2023-08-16 18:44:52 +0300

    add explore offers page, search by tutor name component, student private lesson component

---
+++ SpaceToStudy/ui/pages/explore_offers/explore_offers_page.py
@@ -0,0 +1,36 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+from SpaceToStudy.ui.pages.base_page import BasePage
+from SpaceToStudy.ui.pages.explore_offers.student_private_lesson_component import StudentPrivateLessonComponent
+
+STUDENT_FOR_PRIVATE_LESSONS_BLOCK = (By.XPATH, "/html/body/div/div/div[2]/div[2]/div[1]")
+TITLE = (By.XPATH, "/html/body/div/div/div[2]/div[2]/div[2]/p")
+TEXT = (By.XPATH, "/html/body/div/div/div[2]/div[2]/div[2]/span")
+BACK_TO_ALL_SUBJECT = (By.XPATH, "/html/body/div/div/div[2]/div[2]/div[3]/a")
+SEARCH_BY_TUTOR_NAME_BLOCK = (By.XPATH, "/html/body/div/div/div[2]/div[2]/div[4]/div")
+
+
+class ExploreOffersPage(BasePage):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self._student_for_private_lessons_block = None
+
+    def get_student_for_private_lessons_block(self):
+        node = self.driver.find_element(*STUDENT_FOR_PRIVATE_LESSONS_BLOCK)
+        if not self._student_for_private_lessons_block:
+            self._student_for_private_lessons_block = StudentPrivateLessonComponent(node)
+            return self._student_for_private_lessons_block
+
+    def get_title(self) -> WebElement:
+        return self.driver.find_element(*TITLE)
+
+    def get_test(self) -> WebElement:
+        return self.driver.find_element(*TEXT)
+
+    def get_back_to_all_subject(self) -> WebElement:
+        return self.driver.find_element(*BACK_TO_ALL_SUBJECT)
+
+    def click_back_to_all_subject(self):
+        return self.get_back_to_all_subject().click()
