9072fb3ad0eaee42df6281f09367bc88cbbc1c6c
Natali-natali <rozdilska.n@gmail.com>
2023-08-17 12:38:23 +0300

    fix component

--- SpaceToStudy/ui/pages/home_page/home_student.py
+++ SpaceToStudy/ui/pages/home_page/home_student.py
@@ -14,11 +14,7 @@ BUTTON_FIND_TUTOR = (By.XPATH, "//a[contains(text(), 'Find tutor')]")
 
 SEARCH_INPUT_BLOCK = (By.XPATH, "/html/body/div/div/div[2]/div/div[1]/div/div[2]")
 
-HOW_IT_WORKS_BLOCK_STUDENT = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[3]/div[1]")
-HOW_IT_WORKS_BLOCK_SELECT_A_TUTOR_STUDENT = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[3]/div[2]/div[1]")
-HOW_IT_WORKS_BLOCK_SEND_REQUEST_STUDENT = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[3]/div[2]/div[2]")
-HOW_IT_WORKS_BLOCK_START_LEARNING_STUDENT = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[3]/div[2]/div[3]")
-HOW_IT_WORKS_BLOCK_WRITE_FEEDBACK_STUDENT = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[3]/div[2]/div[4]")
+HOW_IT_WORKS_BLOCK_STUDENT = (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[3]")
 
 
 class HomePageStudent(BasePage):
@@ -29,10 +25,7 @@ class HomePageStudent(BasePage):
         self._button_go_to_categories = None
         self._button_find_tutor = None
         self._how_it_works_block_student = None
-        self._select_a_tutor_student = None
-        self._send_request_student = None
-        self._start_learning_student = None
-        self._write_feedback_student = None
+
 
     def get_categories(self) -> tuple[CategoryComponent]:
         if self._categories is None:
@@ -73,27 +66,5 @@ class HomePageStudent(BasePage):
             self._how_it_works_block_student = HowItWorksComponentStudent(node)
         return self._how_it_works_block_student
 
-    def get_select_a_tutor_items_student(self) -> WebElement:
-        if not self._select_a_tutor_student:
-            node = self.driver.find_element(*HOW_IT_WORKS_BLOCK_SELECT_A_TUTOR_STUDENT)
-            self._select_a_tutor_student = HowItWorksComponentStudent(node)
-        return self._select_a_tutor_student
 
-    def get_send_request_items_student(self) -> WebElement:
-        if not self._send_request_student:
-            node = self.driver.find_element(*HOW_IT_WORKS_BLOCK_SEND_REQUEST_STUDENT)
-            self._send_request_student = HowItWorksComponentStudent(node)
-        return self._send_request_student
-
-    def get_start_learning_items_student(self) -> WebElement:
-        if not self._start_learning_student:
-            node = self.driver.find_element(*HOW_IT_WORKS_BLOCK_START_LEARNING_STUDENT)
-            self._start_learning_student = HowItWorksComponentStudent(node)
-        return self._start_learning_student
-
-    def get_write_feedback_items_student(self) -> WebElement:
-        if not self._write_feedback_student:
-            node = self.driver.find_element(*HOW_IT_WORKS_BLOCK_WRITE_FEEDBACK_STUDENT)
-            self._write_feedback_student = HowItWorksComponentStudent(node)
-        return self._write_feedback_student
 
--- SpaceToStudy/ui/pages/home_page/how_it_works_component_student.py
+++ SpaceToStudy/ui/pages/home_page/how_it_works_component_student.py
@@ -3,12 +3,24 @@ from selenium.webdriver.remote.webelement import WebElement
 
 from SpaceToStudy.ui.pages.base_component import BaseComponent
 
-BLOCK_NAME = (By.XPATH, "./p")
-FOUR_STEPS = (By.XPATH, "./span")
+BLOCK_NAME = (By.XPATH, "./div[1]/p")
+FOUR_STEPS = (By.XPATH, "./div[1]/p/span")
 
-IMAGE_Student = (By.XPATH, "./img")
-TITLE_Student = (By.XPATH, "/.p")
-DESCRIPTION_Student = (By.XPATH, "./span")
+ITEM_SELECT_A_TUTOR_IMG = (By.XPATH, "./div[2]/div[1]/img")
+ITEM_SELECT_A_TUTOR_TITLE = (By.XPATH, "./div[2]/div[1]/div/p")
+ITEM_SELECT_A_TUTOR_DESCRIPTION = (By.XPATH, "./div[2]/div[1]/div/p/span")
+
+ITEM_SEND_REQUEST_IMG = (By.XPATH, "./div[2]/div[2]/img")
+ITEM_SEND_REQUEST_TITLE = (By.XPATH, "./div[2]/div[2]/div/p")
+ITEM_SEND_REQUEST_DESCRIPTION = (By.XPATH, "./div[2]/div[2]/div/p/span")
+
+ITEM_START_LEARNING_IMG = (By.XPATH, "./div[2]/div[3]/img")
+ITEM_START_LEARNING_TITLE = (By.XPATH, "./div[2]/div[3]/div/p")
+ITEM_START_LEARNING_DESCRIPTION = (By.XPATH, "./div[2]/div[3]/div/p/span")
+
+ITEM_WRITE_FEEDBACK_IMG = (By.XPATH, "./div[2]/div[4]/img")
+ITEM_WRITE_FEEDBACK_TITLE = (By.XPATH, "./div[2]/div[4]/div/p")
+ITEM_WRITE_FEEDBACK_DESCRIPTION = (By.XPATH, "./div[2]/div[4]/div/p/span")
 
 
 class HowItWorksComponentStudent(BaseComponent):
@@ -16,9 +28,18 @@ class HowItWorksComponentStudent(BaseComponent):
         super().__init__(node)
         self._block_name_student = None
         self._four_steps = None
-        self._image_student = None
-        self._title_student = None
-        self._description_student = None
+        self._img_select_a_tutor_student = None
+        self._title_select_a_tutor_student = None
+        self._description_select_a_tutor_student = None
+        self._img_send_request_student = None
+        self._title_send_request_student = None
+        self._description_send_request_student = None
+        self._img_start_learning_student = None
+        self._title_start_learning_student = None
+        self._description_start_learning_student = None
+        self._img_write_feedback_student = None
+        self._title_write_feedback_student = None
+        self._description_write_feedback_student = None
 
     def get_block_name_student(self) -> str:
         if not self._block_name_student:
@@ -30,17 +51,62 @@ class HowItWorksComponentStudent(BaseComponent):
             self._four_steps = self.node.find_element(*FOUR_STEPS)
         return self._four_steps.text
 
-    def get_image_student(self) -> WebElement:
-        if not self._image_student:
-            self._image_student = self.node.find_element(*IMAGE_Student)
-        return self._image_student
+    def get_image_select_a_tutor_student(self) -> WebElement:
+        if not self._img_select_a_tutor_student:
+            self._img_select_a_tutor_student = self.node.find_element(*ITEM_SELECT_A_TUTOR_IMG)
+        return self._img_select_a_tutor_student
 
-    def get_name_student(self) -> str:
-        if not self._title_student:
-            self._title_student = self.node.find_element(*TITLE_Student)
-        return self._title_student.text
+    def get_title_select_a_tutor_student(self) -> str:
+        if not self._title_select_a_tutor_student:
+            self._title_select_a_tutor_student = self.node.find_element(*ITEM_SELECT_A_TUTOR_TITLE)
+        return self._title_select_a_tutor_student.text
 
-    def get_description_student(self) -> str:
-        if not self._description_student:
-            self._description_student = self.node.find_element(*DESCRIPTION_Student)
-        return self._description_student.text
+    def get_description_select_a_tutor_student(self) -> str:
+        if not self._description_select_a_tutor_student:
+            self._description_select_a_tutor_student = self.node.find_element(*ITEM_SELECT_A_TUTOR_DESCRIPTION)
+        return self._description_select_a_tutor_student.text
+
+    def get_image_send_request_student(self) -> WebElement:
+        if not self._img_send_request_student:
+            self._img_send_request_student = self.node.find_element(*ITEM_SEND_REQUEST_IMG)
+        return self._img_send_request_student
+
+    def get_title_send_request_student(self) -> str:
+        if not self._title_send_request_student:
+            self._title_send_request_student = self.node.find_element(*ITEM_SEND_REQUEST_TITLE)
+        return self._title_send_request_student.text
+
+    def get_description_send_request_student(self) -> str:
+        if not self._description_send_request_student:
+            self._description_send_request_student = self.node.find_element(*ITEM_SEND_REQUEST_DESCRIPTION)
+        return self._description_send_request_student.text
+
+    def get_image_start_learning_student(self) -> WebElement:
+        if not self._img_start_learning_student:
+            self._img_start_learning_student = self.node.find_element(*ITEM_START_LEARNING_IMG)
+        return self._img_start_learning_student
+
+    def get_title_start_learning_student(self) -> str:
+        if not self._title_start_learning_student:
+            self._title_start_learning_student = self.node.find_element(*ITEM_START_LEARNING_TITLE)
+        return self._title_start_learning_student.text
+
+    def get_description_start_learning_student(self) -> str:
+        if not self._description_start_learning_student:
+            self._description_start_learning_student = self.node.find_element(*ITEM_START_LEARNING_DESCRIPTION)
+        return self._description_start_learning_student.text
+
+    def get_image_write_feedback_student(self) -> WebElement:
+        if not self._img_write_feedback_student:
+            self._img_write_feedback_student = self.node.find_element(*ITEM_WRITE_FEEDBACK_IMG)
+        return self._img_write_feedback_student
+
+    def get_title_write_feedback_student(self) -> str:
+        if not self._title_write_feedback_student:
+            self._title_write_feedback_student = self.node.find_element(*ITEM_WRITE_FEEDBACK_TITLE)
+        return self._title_write_feedback_student.text
+
+    def get_description_write_feedback_student(self) -> str:
+        if not self._description_write_feedback_student:
+            self._description_write_feedback_student = self.node.find_element(*ITEM_WRITE_FEEDBACK_DESCRIPTION)
+        return self._description_write_feedback_student.text
