836eced58dc0ddd71a2e4098456e728d4a93c4f1
Liubomyr Halamaha <galamagal86@gmail.com>
2023-08-25 14:59:01 +0300

    init test runners

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # SpaceToStudyTA
+
+Create a `.env` file in the root of your project:
+
+```dosini
+BASE_URL=base_url
+
+STUDENT_EMAIL=student@test.com
+STUDENT_PASSWORD=student_password
+STUDENT_FIRST_NAME=student
+STUDENT_LAST_NAME=student
+
+TUTOR_EMAIL=tutor@test.com
+TUTOR_PASSWORD=tutor_password
+TUTOR_FIRST_NAME=tutor
+TUTOR_LAST_NAME=tutor
+```
---
+++ tests/test_runners.py
@@ -0,0 +1,39 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from value_pvider import ValueProvider
+
+IMPLICITLY_WAIT = 5
+
+
+class BaseTestRunner(unittest.TestCase):
+
+    def setUp(self):
+        self._init_driver()
+
+    def _init_driver(self):
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+        self.driver.maximize_window()
+        self.addCleanup(self.driver.quit)
+        self.driver.get(ValueProvider.get_base_url())
+
+    def _login(self, email: str, password: str):
+        pass
+
+
+class TestRunnerWithStudent(BaseTestRunner):
+    def setUp(self):
+        self._init_driver()
+        self._login(ValueProvider.get_student_email(),
+                    ValueProvider.get_student_password())
+
+
+class TestRunnerWithTutor(BaseTestRunner):
+    def setUp(self):
+        self._init_driver()
+        self._login(ValueProvider.get_tutor_email(),
+                    ValueProvider.get_tutor_password())
