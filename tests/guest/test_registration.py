7c8fe38ae8bbe9768e38fb0d3d3047ae33f52842
Natali-natali <rozdilska.n@gmail.com>
2023-08-27 15:11:26 +0300

    update

--- tests/guest/test_registration.py
+++ tests/guest/test_registration.py
@@ -9,6 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from SpaceToStudy.ui.pages.home_page.home_guest import HomePageGuest
 from SpaceToStudy.ui.pages.home_page.home_student import HomePageStudent
 from SpaceToStudy.ui.pages.login_modal.login_modal import LoginModal
+from SpaceToStudy.ui.pages.sign_up_modal.sign_up_modal import RegistrationModal
 from tests.test_runners import BaseTestRunner
 
 
@@ -72,6 +73,21 @@ class RegistrationTestCase(BaseTestRunner):
                        .get_individual_time()
                        .is_expanded())
         self.assertTrue(is_expanded)
+
+    def test_registration_password_without_alphabetic_numeric_character(self):
+        home = HomePageGuest(self.driver)
+        registration = RegistrationModal(self.driver)  # NODE OR DRIVER?
+        home.get_button_get_started_for_free().click()
+        (home
+            .get_card_share_your_experience()
+            .click_btn())
+        registration.set_first_name("Nata")
+        registration.set_last_name("Nata")
+        registration.set_email("rozdilska.n@gmail.com")
+        registration.set_password("@#$%//////")
+        registration.click_password_icon()
+        message = registration.get_password_error_message()
+        self.assertEquals(message, "Password must contain at least one alphabetic and one numeric character")
 
 
 if __name__ == '__main__':
