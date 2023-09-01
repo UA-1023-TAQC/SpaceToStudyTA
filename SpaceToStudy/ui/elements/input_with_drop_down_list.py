e0bd3c7236dc422144bad1817d1dfdfa711bc5be
Volodumur Besarab <volodumur911@gmail.com>
2023-09-01 12:41:26 +0300

    Resolve conversation

--- SpaceToStudy/ui/elements/input_with_drop_down_list.py
+++ SpaceToStudy/ui/elements/input_with_drop_down_list.py
@@ -7,22 +7,21 @@ from SpaceToStudy.ui.elements.input import Input
 class InputDropDownList(Input):
     def __init__(self, node: WebElement):
         super().__init__(node)
+        self.default_press_count = 1
 
-    def press_down_button(self, press_count):
-        input_field = self.get_input()
-        input_field.click()
-        for _ in range(press_count):
-            input_field.send_keys(Keys.DOWN)
+    def press_down_button(self, press_count: int):
+        self.default_press_count = press_count
+        for _ in range(self.default_press_count):
+            self.get_input().send_keys(Keys.DOWN)
         return self
 
-    def press_up_button(self, press_count):
-        input_field = self.get_input()
-        input_field.click()
-        for _ in range(press_count):
-            input_field.send_keys(Keys.UP)
+    def press_up_button(self, press_count: int):
+        self.default_press_count = press_count
+        self.get_input().click()
+        for _ in range(self.default_press_count):
+            self.get_input().send_keys(Keys.UP)
         return self
 
     def press_enter_button(self):
-        input_field = self.get_input()
-        input_field.send_keys(Keys.ENTER)
+        self.get_input().send_keys(Keys.ENTER)
         return self
