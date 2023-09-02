10233828e68514f683095c498309c2493c39fa94
Natali-natali <rozdilska.n@gmail.com>
2023-09-02 16:45:33 +0300

    add menu items

--- SpaceToStudy/ui/pages/header/menu_items.py
+++ SpaceToStudy/ui/pages/header/menu_items.py
@@ -4,12 +4,19 @@ from selenium.webdriver.remote.webelement import WebElement
 from SpaceToStudy.ui.pages.base_component import BaseComponent
 
 MY_OFFERS = (By.XPATH, "./ul/a[3]")
+MY_PROFILE = (By.XPATH, "./ul/a[1]")
+MY_COOPERATION = (By.XPATH, "./ul/a[2]")
+LOG_OUT = (By.XPATH, "./ul/a[4]")
+
 
 class MenuItems(BaseComponent):
 
     def __init__(self, node):
         super().__init__(node)
         self._my_offers = None
+        self._my_profile = None
+        self._my_cooperation = None
+        self._log_out = None
 
     def menu_items_my_offers(self) -> WebElement:
         if not self._my_offers:
@@ -18,4 +25,28 @@ class MenuItems(BaseComponent):
 
     def click_menu_items_my_offers(self):
         self.menu_items_my_offers().click()
+
+    def menu_items_my_profile(self) -> WebElement:
+        if not self._my_profile:
+            self._my_profile = self.node.find_element(*MY_OFFERS)
+        return self._my_profile
+
+    def click_menu_items_my_profile(self):
+        self.menu_items_my_profile().click()
+
+    def menu_items_my_cooperation(self) -> WebElement:
+        if not self._my_cooperation:
+            self._my_cooperation = self.node.find_element(*MY_OFFERS)
+        return self._my_cooperation
+
+    def click_menu_items_my_cooperation(self):
+        self.menu_items_my_cooperation().click()
+
+    def menu_items_log_out(self) -> WebElement:
+        if not self._log_out:
+            self._log_out = self.node.find_element(*MY_OFFERS)
+        return self._log_out
+
+    def click_menu_items_log_out(self):
+        self.menu_items_log_out().click()
 
