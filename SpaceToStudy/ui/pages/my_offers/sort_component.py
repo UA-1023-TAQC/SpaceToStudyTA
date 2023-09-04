8a460a2bd88cb45f5788c211e3664fba9d42ea9c
Natali-natali <rozdilska.n@gmail.com>
2023-09-04 17:42:05 +0300

    update data

--- SpaceToStudy/ui/pages/my_offers/sort_component.py
+++ SpaceToStudy/ui/pages/my_offers/sort_component.py
@@ -12,6 +12,7 @@ class SortComponent(BaseComponent):
     def __init__(self, node: WebElement):
         super().__init__(node)
         self._low_high = None
+        self._high_low = None
 
     def get_low_high(self) -> WebElement:
         if not self._low_high:
@@ -22,9 +23,9 @@ class SortComponent(BaseComponent):
         self.get_low_high().click()
 
     def get_high_low(self) -> WebElement:
-        if not self._low_high:
-            self._low_high = self.node.find_element(*HIGH_LOW)
-        return self._low_high
+        if not self._high_low:
+            self._high_low = self.node.find_element(*HIGH_LOW)
+        return self._high_low
 
     def click_high_low(self):
         self.get_high_low().click()
