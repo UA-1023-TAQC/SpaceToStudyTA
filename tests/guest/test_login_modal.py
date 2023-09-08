4cf0231d8198dab24613e69b123d3513bc716bf2
Illia Nakonechnyi <realpeople2807@gmail.com>
2023-09-08 15:03:57 +0300

    added test_user_access_to_personal_cabinet, change xpath sort_title

--- tests/guest/test_login_modal.py
+++ tests/guest/test_login_modal.py
@@ -1,6 +1,10 @@
+from time import sleep
+
 from SpaceToStudy.ui.pages.header.header_unauthorized_component import HeaderUnauthorizedComponent
+from SpaceToStudy.ui.pages.home_page.home_guest import HomePageGuest
 from SpaceToStudy.ui.pages.login_modal.login_modal import LoginModal
 from tests.test_runners import BaseTestRunner
+from tests.value_provider import ValueProvider
 
 
 class LoginModalTestCase(BaseTestRunner):
@@ -12,3 +16,20 @@ class LoginModalTestCase(BaseTestRunner):
                          button.get_value_css_property("background-color"),
                          "Button background color is not as expected")
         self.assertFalse(button.is_enabled_button(), "The button must be disabled")
+
+    def test_user_access_to_personal_cabinet(self):
+        login_modal = (HomePageGuest(self.driver)
+                       .get_header()
+                       .click_login_btn())
+        self.assertTrue(login_modal.get_title_text, "Welcome back")
+        authorize = (LoginModal(self.driver)
+                     .set_email(ValueProvider.get_tutor_email())
+                     .set_password(ValueProvider.get_tutor_password())
+                     # .click_login_button()
+                     )
+        sleep(1)
+        self.assertEqual("rgba(38, 50, 56, 1)",
+                         authorize.get_login_button().get_value_css_property("background-color"))
+        # self.assertTrue(authorize.get)
+        # self.assertTrue()
+
