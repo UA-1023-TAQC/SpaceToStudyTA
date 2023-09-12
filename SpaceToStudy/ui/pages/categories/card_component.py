fea2bd5d3f38a17aae6389989666d002f13fbb5e
Anastasiia <maschenkonastya@gmail.com>
2023-09-12 15:11:43 +0300

    Added categories/card_component.py

---
+++ SpaceToStudy/ui/pages/categories/card_component.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+
+from SpaceToStudy.ui.pages.base_component import BaseComponent
+
+TITLE = (By.XPATH, "./div/p")
+OFFERS = (By.XPATH, "./div/span")
+
+class CardComponent(BaseComponent):
+
+    def __init__(self, node):
+        super().__init__(node)
+        self._title = None
+        self._offers = None
+
+    def get_title(self) -> str:
+        if not self._title:
+            self._title = self.node.find_element(*TITLE)
+        return self._title.text
+
+    def get_offers(self) -> str:
+        if not self._offers:
+            self._offers = self.node.find_element(*OFFERS)
+        return self._offers.text
+
+    def click_card(self):
+        self.node.click()
+        return self
