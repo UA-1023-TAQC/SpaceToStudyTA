ffd0fb5cfb12bc206618af52afa3bb6b08c9b55a
Natali-natali <rozdilska.n@gmail.com>
2023-09-16 16:32:23 +0300

    test_the_collapse_block_ui_text

--- tests/guest/test_home_page.py
+++ tests/guest/test_home_page.py
@@ -38,3 +38,43 @@ class HomePageTestCase(BaseTestRunner):
                        .get_checkbox_share_your_experience()
                        .value_of_css_property("color"))
         self.assertEqual("rgba(96, 125, 139, 1)", block_share)
+
+    def test_the_collapse_block_ui_text(self):
+        flexible_location = "Flexible Location"
+        individual_time = "Individual Time"
+        free_choice = "Free Choice of Tutors"
+        digital_communication = "Digital Communication"
+        description = ("It is a long established fact that a reader will be distracted by the readable content"
+                       " of a page when looking at its layout.")
+        title_fl = (HomePageGuest(self.driver)
+                    .get_flexible_location()
+                    .get_title())
+        self.assertIn(flexible_location, title_fl)
+        description_fl = (HomePageGuest(self.driver)
+                          .get_flexible_location().get_description())
+        self.assertIn(description_fl, description)
+        title_it = (HomePageGuest(self.driver)
+                    .get_individual_time()
+                    .get_title())
+        self.assertIn(individual_time, title_it)
+        description_it = (HomePageGuest(self.driver)
+                          .get_individual_time()
+                          .get_description())
+        self.assertIn(description_it, description)
+        title_fc = (HomePageGuest(self.driver)
+                    .get_free_choice_of_tutors()
+                    .get_title())
+        self.assertIn(free_choice, title_fc)
+        description_fc = (HomePageGuest(self.driver)
+                          .get_free_choice_of_tutors()
+                          .get_description())
+        self.assertIn(description_fc, description)
+        title_dc = (HomePageGuest(self.driver)
+                    .get_digital_communication()
+                    .get_title())
+        self.assertIn(digital_communication, title_dc)
+        description_dc = (HomePageGuest(self.driver)
+                          .get_digital_communication()
+                          .get_description())
+        self.assertIn(description_dc, description)
+
