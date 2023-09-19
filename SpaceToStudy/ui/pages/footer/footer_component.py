4ed0a085b1747d01760f94474bee70670d4b14d1
Illia Nakonechnyi <92325403+Naccarato21@users.noreply.github.com>
2023-09-19 19:51:03 +0300

    added allure steps for: footer_component.py header_authorized_component.py header_component.py (steps №9)

--- SpaceToStudy/ui/pages/footer/footer_component.py
+++ SpaceToStudy/ui/pages/footer/footer_component.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
@@ -16,26 +17,31 @@ class Footer(BaseComponent):
         self._term_of_use = None
         self._all_rights = None
 
+    @allure.step("Get privacy policy text on the footer")
     def get_privacy_policy_text(self) -> str:
         if not self._privacy_policy:
             self._privacy_policy = self.node.find_element(*PRIVACY_POLICY)
         return self._privacy_policy.text
 
+    @allure.step("Click privacy policy link on the footer")
     def click_privacy_policy(self):
         if not self._privacy_policy:
             self._privacy_policy = self.node.find_element(*PRIVACY_POLICY)
             self._privacy_policy.click()
 
+    @allure.step("Get term of use text on the footer")
     def get_term_of_use_text(self) -> str:
         if not self._term_of_use:
             self._term_of_use = self.node.find_element(*TERM_OF_USE)
         return self._term_of_use.text
 
+    @allure.step("Click term of use link on the footer")
     def click_term_of_use(self):
         if not self._term_of_use:
             self._term_of_use = self.node.find_element(*TERM_OF_USE)
             self._term_of_use.click()
 
+    @allure.step("Get all rights span text on the footer")
     def get_all_rights_text(self) -> str:
         if not self._all_rights:
             self._all_rights = self.node.find_element(*ALL_RIGHTS)
