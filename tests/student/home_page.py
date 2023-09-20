05b54f1ecdf5336f22115b0a7afd1f3455a0f053
Natali-natali <rozdilska.n@gmail.com>
2023-09-20 10:28:41 +0300

    add assert

--- tests/student/home_page.py
+++ tests/student/home_page.py
@@ -31,6 +31,9 @@ class TestHomePageStudent(TestRunnerWithStudent):
                          .get_list_of_offers_inline_card())
         for result in search_result:
             self.assertIn("Freestyle and breakdance", result.get_offer_title())
+        count_of_filters = (ExploreOffersPage(self.driver)
+                            .get_filtering_and_sorting_block()
+                            .get_filter_quantity_number())
         tutors_offers_is_active = (ExploreOffersPage(self.driver)
                                    .get_filtering_and_sorting_block()
                                    .get_tutors_offers()
@@ -39,6 +42,7 @@ class TestHomePageStudent(TestRunnerWithStudent):
                                            .get_filtering_and_sorting_block()
                                            .get_students_requests()
                                            .value_of_css_property("color"))
+        self.assertEqual(count_of_filters, 1)
         self.assertEqual(tutors_offers_is_active, "rgba(38, 50, 56, 1)")
         self.assertEqual(students_requests_is_not_active, "rgba(96, 125, 139, 1)")
 
