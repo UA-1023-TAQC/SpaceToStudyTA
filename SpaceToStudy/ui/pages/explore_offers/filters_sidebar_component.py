be02f428da9349300a53006ed4320c7146b14b64
zakotiukk <zakotiukk@gmail.com>
2023-09-20 12:07:32 +0300

    added allure steps 6

--- SpaceToStudy/ui/pages/explore_offers/filters_sidebar_component.py
+++ SpaceToStudy/ui/pages/explore_offers/filters_sidebar_component.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver import ActionChains
 from time import sleep
 
@@ -63,37 +64,46 @@ class FiltersSidebarComponent(BaseComponent):
         super().__init__(node)
         self._list_of_languages = None
 
+    @allure.step("Click on the level beginner checkbox")
     def click_level_beginner_checkbox(self):
         self.node.find_element(*LEVEL_BEGINNER_CHECKBOX).click()
         return self
 
+    @allure.step("Click on the level intermediate checkbox")
     def click_level_intermediate_checkbox(self):
         self.node.find_element(*LEVEL_INTERMEDIATE_CHECKBOX).click()
         return self
 
+    @allure.step("Click on the level advanced checkbox")
     def click_level_advanced_checkbox(self):
         self.node.find_element(*LEVEL_ADVANCED_CHECKBOX).click()
         return self
 
+    @allure.step("Click on the level test preparation checkbox")
     def click_level_test_preparation_checkbox(self):
         self.node.find_element(*LEVEL_TEST_PREPARATION_CHECKBOX).click()
         return self
 
+    @allure.step("Click on the level professional checkbox")
     def click_level_professional_checkbox(self):
         self.node.find_element(*LEVEL_PROFESSIONAL_CHECKBOX).click()
         return self
 
+    @allure.step("Click on the level specialized checkbox")
     def click_level_specialized_checkbox(self):
         self.node.find_element(*LEVEL_SPECIALIZED_CHECKBOX).click()
         return self
 
+    @allure.step("Click on the language input")
     def click_language_input(self):
         self.node.find_element(*LANGUAGE_INPUT).click()
         return self
 
+    @allure.step("Get language input")
     def get_language_input(self):
         return self.node.find_element(*LANGUAGE_INPUT)
 
+    @allure.step("Get list of languages")
     def get_list_of_languages(self) -> list:
         languages = self.node.find_elements(*LANGUAGE_LIST)
         self._list_of_languages = []
@@ -101,6 +111,7 @@ class FiltersSidebarComponent(BaseComponent):
             self._list_of_languages.append(language.text)
         return self._list_of_languages
 
+    @allure.step("Set language input")
     def set_language_input(self, language):
         languages = self.node.find_elements(*LANGUAGE_LIST)
         for item in languages:
@@ -109,36 +120,44 @@ class FiltersSidebarComponent(BaseComponent):
                 break
         return self
 
+    @allure.step("Click on the native speaker checkbox")
     def click_native_speaker_checkbox(self):
         self.node.find_element(*NATIVE_SPEAKER_CHECKBOX).click()
         return self
 
+    @allure.step("Get the lowest value on slider")
     def get_lowest_value_on_slider(self):
         return float(self.node.find_element(*PRICE_LOWEST_VALUE_ON_SLIDER).text)
 
+    @allure.step("Get the highest value on slider")
     def get_highest_value_on_slider(self):
         return float(self.node.find_element(*PRICE_HIGHEST_VALUE_ON_SLIDER).text)
 
+    @allure.step("Get the lowest value input")
     def get_lowest_value_input(self):
         return float(self.node.find_element(*PRICE_LOWEST_VALUE_INPUT)
                      .get_attribute('value'))
 
+    @allure.step("Get the highest value input")
     def get_highest_value_input(self):
         return float(self.node.find_element(*PRICE_HIGHEST_VALUE_INPUT)
                      .get_attribute('value'))
 
+    @allure.step("Set the lowest value input")
     def set_lowest_value_input(self, value):
         self.node.find_element(*PRICE_LOWEST_VALUE_INPUT) \
             .send_keys(Keys.CONTROL + "a" + Keys.BACKSPACE)
         self.node.find_element(*PRICE_LOWEST_VALUE_INPUT).send_keys(value)
         return self
 
+    @allure.step("Set the highest value input")
     def set_highest_value_input(self, value):
         self.node.find_element(*PRICE_HIGHEST_VALUE_INPUT)\
             .send_keys(Keys.CONTROL + "a" + Keys.BACKSPACE)
         self.node.find_element(*PRICE_HIGHEST_VALUE_INPUT).send_keys(value)
         return self
 
+    @allure.step("Drag left slider")
     def drag_left_slider(self, pixels_to_the_right):
         slider = self.node.find_element(*PRICE_LEFT_SLIDER)
         ActionChains(self.node.parent) \
@@ -146,6 +165,7 @@ class FiltersSidebarComponent(BaseComponent):
             .perform()
         return self
 
+    @allure.step("Drag right slider")
     def drag_right_slider(self, pixels_to_the_left):
         slider = self.node.find_element(*PRICE_RIGHT_SLIDER)
         ActionChains(self.node.parent) \
@@ -153,36 +173,44 @@ class FiltersSidebarComponent(BaseComponent):
             .perform()
         return self
 
+    @allure.step("Click on any rating radio button")
     def click_any_rating_radio_btn(self):
         self.node.find_element(*RATING_ANY_RATING_RADIOBTN).click()
         return self
 
+    @allure.step("Click on the 5 stars radio button")
     def click_5_stars_radio_btn(self):
         self.node.find_element(*RATING_5_STARS_RADIOBTN).click()
         return self
 
+    @allure.step("Click on the 4 stars and above radio button")
     def click_4_and_above_radio_btn(self):
         self.node.find_element(*RATING_4_AND_ABOVE_RADIOBTN).click()
         return self
 
+    @allure.step("Click on the 3 stars and above radio button")
     def click_3_and_above_radio_btn(self):
         self.node.find_element(*RATING_3_AND_ABOVE_RADIOBTN).click()
         return self
 
+    @allure.step("Set search by name input")
     def set_search_by_name_input(self, name):
         self.node.find_element(*SEARCH_BY_NAME_INPUT).send_keys(name)
         return self
 
+    @allure.step("Click on the apply filters button")
     def click_apply_filters_btn(self):
         sleep(0.5)
         from SpaceToStudy.ui.pages.explore_offers.explore_offers_page import ExploreOffersPage
         self.node.find_element(*APPLY_FILTERS_BTN).click()
         return ExploreOffersPage(self.node.parent)
 
+    @allure.step("Click on the clear filters button")
     def click_clear_filters_btn(self):
         self.node.find_element(*CLEAR_FILTERS_BTN).click()
         return self
 
+    @allure.step("Click on the close button")
     def click_close_button(self):
         from SpaceToStudy.ui.pages.explore_offers.explore_offers_page import ExploreOffersPage
         self.node.find_element(*CLOSE_BUTTON).click()
