89145c86b4b5f9e9c92dd68dad19c18afaeeffbd
Natali-natali <rozdilska.n@gmail.com>
2023-09-25 12:54:21 +0300

    test_open_student_registration_modal_at_what_can_you_do_block

--- SpaceToStudy/ui/pages/header/navigate_component.py
+++ SpaceToStudy/ui/pages/header/navigate_component.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
 from SpaceToStudy.ui.pages.base_component import BaseComponent
+from SpaceToStudy.ui.pages.home_page.home_guest import HomePageGuest
 
 NAME = (By.XPATH, "./a")
 
@@ -15,7 +16,7 @@ class NavigateComponent(BaseComponent):
     @allure.step("Click on a navigation link")
     def click(self):
         self.node.click()
-        return self.node
+        return HomePageGuest(self.node.parent)
 
     @allure.step("Get the text of a navigation link")
     def get_name(self) -> str:
