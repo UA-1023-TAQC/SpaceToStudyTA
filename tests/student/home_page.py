cbf6ad995059d8be0639d4c9b2bda874cd88f990
Natali-natali <rozdilska.n@gmail.com>
2023-09-26 18:38:03 +0300

    update

--- tests/student/home_page.py
+++ tests/student/home_page.py
@@ -1,3 +1,5 @@
+import allure
+
 from SpaceToStudy.ui.pages.explore_offers.explore_offers_page import ExploreOffersPage
 from SpaceToStudy.ui.pages.home_page.home_student import HomePageStudent
 from tests.test_runners import TestRunnerWithStudent
@@ -11,7 +13,7 @@ class TestHomePageStudent(TestRunnerWithStudent):
                          .get_search_input()
                          .set_text("Tutor F")
                          .click_find_tutor_btn()
-                         .get_list_of_offers_grid_card())
+                         .get_list_of_offers_inline_card())
         for result in search_result:
             self.assertIn("Tutor F", result.get_person_name())
         tutors_offers_is_active = (ExploreOffersPage(self.driver)
@@ -55,7 +57,7 @@ class TestHomePageStudent(TestRunnerWithStudent):
                          .get_search_input()
                          .set_text("CYBERSECURITY")
                          .click_find_tutor_btn()
-                         .get_list_of_offers_grid_card())
+                         .get_list_of_offers_inline_card())
         for result in search_result:
             self.assertIn("CYBERSECURITY", result.get_subject_label())
         count_of_filters = (ExploreOffersPage(self.driver)
