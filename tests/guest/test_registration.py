57391d1faaa23b93c5381050b4481b386ebd19bf
zakotiukk <zakotiukk@gmail.com>
2023-09-27 21:35:14 +0300

    added test for tutor sign up with invalid data in the last name field

--- tests/guest/test_registration.py
+++ tests/guest/test_registration.py
@@ -139,6 +139,18 @@ class RegistrationTestCase(BaseTestRunner):
             .get_text_title_modal()
         self.assertEqual(title_tutor, "Sign up as a tutor")
 
+    @allure.testcase('https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/315')
+    def test_tutor_sign_up_invalid_data_for_last_name(self):
+        (HomePageGuest(self.driver)
+         .click_started_for_free()
+         .click_become_a_tutor())
+        error = (RegistrationModal(self.driver)
+                 .set_last_name("ajj#&^1234")
+                 .click_i_agree_checkbox()
+                 .get_last_name_error_message())
+        self.assertEqual(error, "This field can contain alphabetic characters only")
+
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
