1eff87062be4516f4fbc071c6ba07a8c6220a2a5
Natali-natali <rozdilska.n@gmail.com>
2023-09-27 18:30:10 +0300

    test_popular_categories_block_ui_button

---
+++ tests/student/popular_categories_block_ui.py
@@ -0,0 +1,27 @@
+import allure
+
+from SpaceToStudy.ui.pages.home_page.home_student import HomePageStudent, BUTTON_GO_TO_CATEGORIES
+from tests.test_runners import TestRunnerWithStudent
+
+
+class PopularCategoriesBlockUI(TestRunnerWithStudent):
+
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/203")
+    def test_popular_categories_block_ui_button(self):
+        button_is_displayed = (HomePageStudent(self.driver)
+                               .get_button_go_to_categories()
+                               .is_displayed())
+        button_text = (HomePageStudent(self.driver)
+                       .get_text_button_go_to_categories())
+        button = self.driver.find_element(*BUTTON_GO_TO_CATEGORIES)
+        button_hover = (HomePageStudent(self.driver)
+                        .hover(button)
+                        .value_of_css_property("background-color"))
+        self.assertEqual("rgba(0, 0, 0, 0.04)", button_hover)
+        page_is_displayed = (HomePageStudent(self.driver)
+                             .click_button_go_to_categories()
+                             .get_categories_block()
+                             .is_displayed())
+        self.assertEqual("Go to categories", button_text)
+        self.assertTrue(button_is_displayed, "Button is not displayed")
+        self.assertTrue(page_is_displayed, "Page is not displayed")
