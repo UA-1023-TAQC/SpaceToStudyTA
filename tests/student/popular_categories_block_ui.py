6506a56434a7aed255541d8653b5f641acef8500
Natali-natali <rozdilska.n@gmail.com>
2023-10-03 22:08:33 +0300

    update

--- tests/student/popular_categories_block_ui.py
+++ tests/student/popular_categories_block_ui.py
@@ -10,7 +10,6 @@ from SpaceToStudy.ui.pages.home_page.home_student import (HomePageStudent,
                                                           CATEGORY_MATH,
                                                           CATEGORY_DANCE,
                                                           CATEGORY_LANGUAGES)
-from SpaceToStudy.ui.pages.home_page.home_student import HomePageStudent, BUTTON_GO_TO_CATEGORIES
 from tests.test_runners import TestRunnerWithStudent
 
 
@@ -61,24 +60,6 @@ class PopularCategoriesBlockUI(TestRunnerWithStudent):
         cursor_dance_el = (HomePageStudent(self.driver).hover(get_dance_el).value_of_css_property("cursor"))
         self.assertEqual("rgba(144, 164, 174, 0.56) 0px 3px 16px 2px", hover_dance_el)
         self.assertEqual("pointer", cursor_dance_el)
-    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/340")
-    def test_popular_categories_block_ui_alignment(self):
-        delta = 13
-        size_window = self.driver.get_window_size()
-        get_width_window = size_window['width']
-        get_block = HomePageStudent(self.driver).get_categories_block()
-        get_width_block = get_block.size['width']
-        left_margin = get_block.location['x'] + delta
-        right_margin = get_width_window - get_width_block - left_margin
-        horizontal_margin_between_elements = 294
-        vertical_margin_between_elements = 136
-        start_coordinate_x = (get_width_window - get_width_block) / 2 - delta
-        start_coordinate_y = 747
-
-        location_x = (HomePageStudent(self.driver).get_categories())
-        for result in location_x[:4]:
-            self.assertEqual(start_coordinate_x, result.node.location['x'])
-            start_coordinate_x = start_coordinate_x + horizontal_margin_between_elements
 
         get_math_el = self.driver.find_element(*CATEGORY_MATH)
         hover_math_el = (HomePageStudent(self.driver).hover(get_math_el).value_of_css_property("box-shadow"))
@@ -86,7 +67,6 @@ class PopularCategoriesBlockUI(TestRunnerWithStudent):
         self.assertEqual("rgba(144, 164, 174, 0.56) 0px 3px 16px 2px", hover_math_el)
         self.assertEqual("pointer", cursor_math_el)
 
-        self.assertEqual(right_margin, left_margin)
         get_languages_el = self.driver.find_element(*CATEGORY_LANGUAGES)
         hover_languages_el = (HomePageStudent(self.driver).hover(get_languages_el).value_of_css_property("box-shadow"))
         cursor_languages_el = (HomePageStudent(self.driver).hover(get_languages_el).value_of_css_property("cursor"))
