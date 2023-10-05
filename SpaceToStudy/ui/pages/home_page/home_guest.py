20c9283e6574d73a28654d0bfa27bd21609c69b7
zakotiukk <zakotiukk@gmail.com>
2023-10-05 12:43:13 +0300

    some changes

--- SpaceToStudy/ui/pages/home_page/home_guest.py
+++ SpaceToStudy/ui/pages/home_page/home_guest.py
@@ -365,10 +365,3 @@ class HomePageGuest(BasePage):
             node = self.driver.find_element(*WHO_WE_ARE_BLOCK)
             self._who_we_are_block = WhoWeAreBlock(node)
         return self._who_we_are_block
-
-    @allure.step("Get button \"get started for free\"")
-    def get_button_get_started_for_free(self) -> WebElement:
-        if not self._button_get_started_for_free:
-            self._button_get_started_for_free = self.driver.find_element(*BUTTON_GET_STARTED_FOR_FREE)
-        return self._button_get_started_for_free
-
