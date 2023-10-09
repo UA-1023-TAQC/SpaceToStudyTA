b2624ecffcf3bbfe341b65e4a8f868d5848a5b9c
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-10-09 18:36:30 +0300

    added allure steps and modified methods

--- SpaceToStudy/ui/pages/home_page/home_tutor.py
+++ SpaceToStudy/ui/pages/home_page/home_tutor.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
@@ -13,26 +14,42 @@ class HomePageTutor(BasePage):
 
     def __init__(self, driver):
         super().__init__(driver)
+        self._title = None
+        self._search_input = None
         self._find_student = None
 
+    @allure.step("Get title")
     def get_title(self) -> WebElement:
-        return self.driver.find_element(*TITLE)
+        if self._title is None:
+            self._title = self.driver.find_element(*TITLE)
+        return self._title
 
+    @allure.step("Get title text")
+    def get_title_text(self) -> str:
+        return self.get_title().text
+
+    @allure.step("Get search input")
     def get_search_input(self) -> WebElement:
-        return self.driver.find_element(*SEARCH_INPUT)
+        if self._search_input is None:
+            self._search_input = self.driver.find_element(*SEARCH_INPUT)
+        return self._search_input
 
-    def get_search_field_placeholder(self) -> str:
+    @allure.step("Get search input placeholder")
+    def get_search_input_placeholder(self) -> str:
         return self.get_search_input().get_attribute("placeholder")
 
+    @allure.step("Set search input text '{text}'")
     def set_search_input_text(self, text):
         self.get_search_input().send_keys(text)
         return self
 
+    @allure.step("Get find student button")
     def get_find_student_btn(self) -> WebElement:
         if self._find_student is None:
             self._find_student = self.driver.find_element(*FIND_STUDENT)
         return self._find_student
 
+    @allure.step("Click 'Find student' button")
     def click_find_student_btn(self) -> ExploreOffersPage:
         self.get_find_student_btn().click()
         return ExploreOffersPage(self.driver)
