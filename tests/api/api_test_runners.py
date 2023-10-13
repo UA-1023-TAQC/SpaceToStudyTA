287fd179ee107c36b998cbb1353e5ffc56e60c32
Liubomyr Halamaha <galamagal86@gmail.com>
2023-10-13 14:37:35 +0300

    update

--- tests/api/api_test_runners.py
+++ tests/api/api_test_runners.py
@@ -1,31 +1,28 @@
 import unittest
-import requests
 
 from SpaceToStudy.api.auth_client import AuthApiClient
 from tests.utils.value_provider import ValueProvider
 
 
 class BaseAPITestRunner(unittest.TestCase):
-    def setUp(self):
-        self.api = AuthApiClient(ValueProvider.get_base_api_url())
-        self.accessToken = None
-
-    def get_response_json(self, url):
-        response = requests.get(url, headers={"Authorization": f"Bearer {self.accessToken}"})
-        return response.json()
+    @classmethod
+    def setUpClass(cls):
+        cls.accessToken = None
 
 
 class APITestRunnerWithStudent(BaseAPITestRunner):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         api = AuthApiClient(ValueProvider.get_base_api_url())
         api.login(ValueProvider.get_student_email(),
                   ValueProvider.get_student_password())
-        self.accessToken = api.accessToken
+        cls.accessToken = api.accessToken
 
 
 class APITestRunnerWithTutor(BaseAPITestRunner):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         api = AuthApiClient(ValueProvider.get_base_api_url())
         api.login(ValueProvider.get_tutor_email(),
                   ValueProvider.get_tutor_password())
-        self.accessToken = api.accessToken
+        cls.accessToken = api.accessToken
--- tests/api/student/test_categories.py
+++ tests/api/student/test_categories.py
@@ -1,68 +1,17 @@
 import allure
 from jsonschema import validate
 
+from SpaceToStudy.api.categories.client import CategoriesApiClient
+from SpaceToStudy.api.categories.schemas import SCHEMA_FOR_ALL_CATEGORIES
 from tests.api.api_test_runners import APITestRunnerWithStudent
 from tests.utils.value_provider import ValueProvider
-
-SCHEMA_FOR_ALL_CATEGORIES = {
-    "type": "object",
-    "properties": {
-        "items": {
-            "type": "array",
-            "items": {
-                "type": "object",
-                "properties": {
-                    "_id": {
-                        "type": "string"
-                    },
-                    "name": {
-                        "type": "string"
-                    },
-                    "appearance": {
-                        "type": "object",
-                        "properties": {
-                            "icon": {
-                                "type": "string"
-                            },
-                            "color": {
-                                "type": "string"
-                            }
-                        },
-                        "required": ["icon", "color"]
-                    },
-                    "totalOffers": {
-                        "type": "object",
-                        "properties": {
-                            "student": {
-                                "type": "integer"
-                            },
-                            "tutor": {
-                                "type": "integer"
-                            }
-                        },
-                        "required": ["student", "tutor"]
-                    },
-                    "createdAt": {
-                        "type": "string"
-                    },
-                    "updatedAt": {
-                        "type": "string"
-                    }
-                },
-                "required": ["_id", "name", "appearance", "totalOffers", "createdAt", "updatedAt"]
-            }
-        },
-        "count": {
-            "type": "integer"
-        }
-    },
-    "required": ["items", "count"]
-}
 
 
 class TestAPICategories(APITestRunnerWithStudent):
 
     @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/388")
     def test_find_all_categories(self):
-        url = f"{ValueProvider.get_base_api_url()}categories"
-        validate(instance=self.get_response_json(url), schema=SCHEMA_FOR_ALL_CATEGORIES)
+        client = CategoriesApiClient(ValueProvider.get_base_api_url(), self.accessToken)
+        response = client.get_categories()
+        self.assertEquals(response.status_code, 200)
+        validate(instance=response.json(), schema=SCHEMA_FOR_ALL_CATEGORIES)
