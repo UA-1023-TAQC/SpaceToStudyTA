39c2c421b10ce59b8041676497bf605837189914
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-10-16 19:28:57 +0300

    Created test for api to find category by id

--- tests/api/student/test_categories.py
+++ tests/api/student/test_categories.py
@@ -2,7 +2,7 @@ import allure
 from jsonschema import validate
 
 from SpaceToStudy.api.categories.client import CategoriesApiClient
-from SpaceToStudy.api.categories.schemas import SCHEMA_FOR_ALL_CATEGORIES
+from SpaceToStudy.api.categories.schemas import SCHEMA_FOR_ALL_CATEGORIES, SCHEMA_CATEGORIES_BY_ID
 from tests.api.api_test_runners import APITestRunnerWithStudent
 from tests.utils.value_provider import ValueProvider
 
@@ -15,3 +15,11 @@ class TestAPICategories(APITestRunnerWithStudent):
         response = client.get_categories()
         self.assertEquals(response.status_code, 200)
         validate(instance=response.json(), schema=SCHEMA_FOR_ALL_CATEGORIES)
+
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/408")
+    def test_get_categories_by_id(self):
+        client = CategoriesApiClient(ValueProvider.get_base_api_url(), self.accessToken)
+        response = client.get_categories_by_id("64884f21fdc2d1a130c24ac0")
+        self.assertEquals(response.status_code, 200)
+        validate(instance=response.json(), schema=SCHEMA_CATEGORIES_BY_ID)
+        self.assertEquals(response.json()["name"], "Music")
