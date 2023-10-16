73a2256ea199030f7393b13955b564c9536ba267
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-10-16 21:48:25 +0300

    created schema for subjects by category id for api

--- SpaceToStudy/api/categories/schemas.py
+++ SpaceToStudy/api/categories/schemas.py
@@ -53,3 +53,51 @@ SCHEMA_FOR_ALL_CATEGORIES = {
     },
     "required": ["items", "count"]
 }
+
+SCHEMA_FOR_SUBJECTS_BY_CATEGORY_ID = {
+  "type": "object",
+  "properties": {
+    "count": {
+      "type": "integer"
+    },
+    "items": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "_id": {
+            "type": "string"
+          },
+          "name": {
+            "type": "string"
+          },
+          "category": {
+            "type": "string"
+          },
+          "totalOffers": {
+            "type": "object",
+            "properties": {
+              "student": {
+                "type": "integer"
+              },
+              "tutor": {
+                "type": "integer"
+              }
+            },
+            "required": ["student", "tutor"]
+          },
+          "createdAt": {
+            "type": "string",
+            "format": "date-time"
+          },
+          "updatedAt": {
+            "type": "string",
+            "format": "date-time"
+          }
+        },
+        "required": ["_id", "name", "category", "totalOffers", "createdAt", "updatedAt"]
+      }
+    }
+  },
+  "required": ["count", "items"]
+}
