09a016a1ec81e66578783aa019a94fb143a4e021
Natali-natali <rozdilska.n@gmail.com>
2023-10-16 12:54:08 +0300

    update

--- SpaceToStudy/api/offers/schemas.py
+++ SpaceToStudy/api/offers/schemas.py
@@ -14,3 +14,137 @@ unauthorized_schema = {
     },
     "required": ["code", "message"],
 }
+
+all_offers_schema = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "title": "Generated schema for Root",
+  "type": "object",
+  "properties": {
+    "count": {
+      "type": "number"
+    },
+    "offers": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "_id": {
+            "type": "string"
+          },
+          "price": {
+            "type": "number"
+          },
+          "proficiencyLevel": {
+            "type": "array",
+            "items": {
+              "type": "string"
+            }
+          },
+          "title": {
+            "type": "string"
+          },
+          "description": {
+            "type": "string"
+          },
+          "languages": {
+            "type": "array",
+            "items": {
+              "type": "string"
+            }
+          },
+          "authorRole": {
+            "type": "string"
+          },
+          "authorFirstName": {
+            "type": "string"
+          },
+          "authorLastName": {
+            "type": "string"
+          },
+          "authorAvgRating": {
+            "type": "number"
+          },
+          "author": {
+            "type": "object",
+            "properties": {
+              "_id": {
+                "type": "string"
+              },
+              "totalReviews": {
+                "type": "object",
+                "properties": {
+                  "student": {
+                    "type": "number"
+                  },
+                  "tutor": {
+                    "type": "number"
+                  }
+                },
+                "required": [
+                  "student",
+                  "tutor"
+                ]
+              },
+              "FAQ": {},
+              "professionalSummary": {
+                "type": "string"
+              },
+              "photo": {
+                "type": "string"
+              }
+            },
+            "required": [
+              "_id",
+              "totalReviews",
+              "FAQ",
+              "professionalSummary",
+              "photo"
+            ]
+          },
+          "subject": {
+            "type": "object",
+            "properties": {
+              "_id": {
+                "type": "string"
+              },
+              "name": {
+                "type": "string"
+              }
+            },
+            "required": [
+              "_id",
+              "name"
+            ]
+          },
+          "category": {
+            "type": "string"
+          },
+          "chatId": {
+            "type": "string"
+          },
+          "FAQ": {}
+        },
+        "required": [
+          "_id",
+          "price",
+          "proficiencyLevel",
+          "title",
+          "description",
+          "languages",
+          "authorRole",
+          "authorFirstName",
+          "authorLastName",
+          "authorAvgRating",
+          "author",
+          "subject",
+          "category",
+          "FAQ"
+        ]
+      }
+    }
+  },
+  "required": [
+    "count",
+    "offers"
+  ]
+}
