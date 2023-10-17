f9ae9f482b0b9760bafd208ecbcc6f37429c417f
Natali-natali <rozdilska.n@gmail.com>
2023-10-17 19:43:11 +0300

    add negative test api offers id

--- tests/api/student/test_offers.py
+++ tests/api/student/test_offers.py
@@ -2,6 +2,7 @@ import allure
 
 from SpaceToStudy.api.offers.client_offers import OffersApiClient
 from SpaceToStudy.api.offers.schemas import ALL_OFFERS_SCHEMA
+from SpaceToStudy.api.schema_for_errors import SCHEMA_FOR_ERRORS
 from tests.api.api_test_runners import APITestRunnerWithStudent
 from tests.utils.value_provider import ValueProvider
 from jsonschema import validate
@@ -15,3 +16,13 @@ class TestOffersApi(APITestRunnerWithStudent):
         response = client.get_offers()
         self.assertEqual(200, response.status_code)
         validate(instance=response.json(), schema=ALL_OFFERS_SCHEMA)
+
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/422#issue-1947831861")
+    def test_find_all_offers(self):
+        client = OffersApiClient(ValueProvider.get_base_api_url(), self.accessToken)
+        response = client.get_offers_by_id("123")
+        self.assertEqual(400, response.status_code)
+        assert response.status_code == 400
+        assert response.json().get('message') == "ID is invalid."
+        assert response.json().get('code') == "INVALID_ID"
+        validate(instance=response.json(), schema=SCHEMA_FOR_ERRORS)
