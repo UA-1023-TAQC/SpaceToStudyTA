589584c869ae2cdfd28931d7c018d38c133a286b
Natali-natali <rozdilska.n@gmail.com>
2023-10-17 19:28:36 +0300

    add test api offers id

--- SpaceToStudy/api/offers/client_offers.py
+++ SpaceToStudy/api/offers/client_offers.py
@@ -13,3 +13,9 @@ class OffersApiClient(BaseAPIClient):
         response = requests.get(url, headers={"Authorization": f"Bearer {self.access_token}"})
         return response
 
+    def get_offers_by_id(self, offers_id):
+        "/ categories / {id}"
+        url = f"{self.url}/{offers_id}"
+        response = requests.get(url, headers={"Authorization": f"Bearer {self.access_token}"})
+        return response
+
--- tests/api/student/test_offers.py
+++ tests/api/student/test_offers.py
@@ -1,7 +1,7 @@
 import allure
 
 from SpaceToStudy.api.offers.client_offers import OffersApiClient
-from SpaceToStudy.api.offers.schemas import ALL_OFFERS_SCHEMA
+from SpaceToStudy.api.offers.schemas import ALL_OFFERS_SCHEMA, SCHEMA_OFFERS_ID
 from tests.api.api_test_runners import APITestRunnerWithStudent
 from tests.utils.value_provider import ValueProvider
 from jsonschema import validate
@@ -15,3 +15,10 @@ class TestOffersApi(APITestRunnerWithStudent):
         response = client.get_offers()
         self.assertEqual(200, response.status_code)
         validate(instance=response.json(), schema=ALL_OFFERS_SCHEMA)
+
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/405#issue-1945106730")
+    def test_find_all_offers(self):
+        client = OffersApiClient(ValueProvider.get_base_api_url(), self.accessToken)
+        response = client.get_offers_by_id("652ea7336fc04ef55bb462cf")
+        self.assertEqual(200, response.status_code)
+        validate(instance=response.json(), schema=SCHEMA_OFFERS_ID)
