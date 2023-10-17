2bd34c581ef82de59bfc3f4fb2e06a2dc0cb6597
headles <bobzakon16@gmail.com>
2023-10-17 10:48:52 +0300

    update

--- tests/api/student/test_auth.py
+++ tests/api/student/test_auth.py
@@ -1,10 +1,12 @@
+import allure
+
 from SpaceToStudy.api.auth_client import AuthApiClient
 from tests.api.api_test_runners import APITestRunnerWithStudent
 from tests.utils.value_provider import ValueProvider
 
 
 class TestAPIAuth(APITestRunnerWithStudent):
-
+    @allure.testcase('https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/415')
     def test_logout(self):
         user = AuthApiClient(ValueProvider.get_base_api_url(), self.accessToken)
         response = user.logout()
