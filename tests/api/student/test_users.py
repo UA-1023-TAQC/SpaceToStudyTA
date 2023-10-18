e102dea3796377a2d35e992ae3316a15012bdc13
Olha Haichenkova <olha.haichenkova@gmail.com>
2023-10-18 13:09:56 +0300

    Moved tests for api/users with code 401 to guest/test_users

--- tests/api/student/test_users.py
+++ tests/api/student/test_users.py
@@ -19,19 +19,6 @@ class TestAPIUsers(APITestRunnerWithStudent):
         self.assertEqual(expected_status_code, response.status_code)
         validate(instance=response.json(), schema=SCHEMA_FOR_ALL_USERS)
 
-    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/402",
-                     "Create test for api/users find all users")
-    def test_find_all_users_unauthorized(self):
-        expected_status_code = 401
-        expected_code = "UNAUTHORIZED"
-        expected_message = "The requested URL requires user authorization."
-        client = UsersApiClient(ValueProvider.get_base_api_url())
-        response = client.get_users()
-        self.assertEqual(expected_status_code, response.status_code)
-        self.assertEqual(expected_code, response.json().get('code'))
-        self.assertEqual(expected_message, response.json().get('message'))
-        validate(instance=response.json(), schema=SCHEMA_FOR_ERRORS)
-
     @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/404",
                      "Create test for api/users Find user by ID ")
     def test_find_user_by_id(self):
@@ -61,21 +48,6 @@ class TestAPIUsers(APITestRunnerWithStudent):
         user_id = "abcdefg"
 
         client = UsersApiClient(ValueProvider.get_base_api_url(), self.accessToken)
-        response = client.get_users_by_id(user_id)
-        self.assertEqual(expected_status_code, response.status_code)
-        self.assertEqual(expected_code, response.json().get('code'))
-        self.assertEqual(expected_message, response.json().get('message'))
-        validate(instance=response.json(), schema=SCHEMA_FOR_ERRORS)
-
-    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/404",
-                     "Create test for api/users Find user by ID ")
-    def test_find_user_by_id_unauthorized(self):
-        expected_status_code = 401
-        expected_code = "UNAUTHORIZED"
-        expected_message = "The requested URL requires user authorization."
-        user_id = "644f6f1777e2551b87786650"
-
-        client = UsersApiClient(ValueProvider.get_base_api_url())
         response = client.get_users_by_id(user_id)
         self.assertEqual(expected_status_code, response.status_code)
         self.assertEqual(expected_code, response.json().get('code'))
