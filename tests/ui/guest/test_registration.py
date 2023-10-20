0086e22b66dcfc373840a754d5cd1710077becff
Viacheslav Korniienko <viacheslav.korniienko@gmail.com>
2023-10-20 14:15:16 +0300

    repeatedly added test [Sign up as tutor popup] Verify that the 'Sing Up' button is active when all fields are filled out with correct data.

--- tests/ui/guest/test_registration.py
+++ tests/ui/guest/test_registration.py
@@ -555,6 +555,20 @@ class RegistrationTestCase(BaseTestRunner):
                                .get_who_we_are_elements())
         for element in who_we_are_elements.values():
             self.assertTrue(element.is_displayed())
+    @allure.testcase('https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/312')
+    def test_tutor_sign_up_button_active_with_correct_data(self):
+        (HomePageGuest(self.driver)
+         .click_started_for_free()
+         .click_become_a_tutor())
+        sign_up_button_state = (RegistrationModal(self.driver)
+                                .set_first_name("Dan")
+                                .set_last_name("Brown")
+                                .set_email("danbrown@example.com")
+                                .set_password("Password123")
+                                .set_confirm_password("Password123")
+                                .click_i_agree_checkbox()
+                                .get_sign_up_btn())
+        self.assertTrue(sign_up_button_state.is_enabled(), "The 'Sign Up' button should be active")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
