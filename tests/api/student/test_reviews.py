82b1da8c9d1e08b904e000e85c0ebdcd844ac351
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-10-24 19:38:12 +0300

    created test for api to get all reviews

---
+++ tests/api/student/test_reviews.py
@@ -0,0 +1,17 @@
+import allure
+from jsonschema import validate
+
+from SpaceToStudy.api.reviews.client import ReviewsApiClient
+from SpaceToStudy.api.reviews.schemas import SCHEMA_FOR_ALL_REVIEWS
+from tests.api.api_test_runners import APITestRunnerWithStudent
+from tests.utils.value_provider import ValueProvider
+
+
+class TestAPIReviews(APITestRunnerWithStudent):
+
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/444")
+    def test_get_all_reviews(self):
+        client = ReviewsApiClient(ValueProvider.get_base_api_url(), self.accessToken)
+        response = client.get_all_reviews(rating=5, skip=1, limit=5)
+        self.assertEqual(200, response.status_code)
+        validate(instance=response.json(), schema=SCHEMA_FOR_ALL_REVIEWS)
