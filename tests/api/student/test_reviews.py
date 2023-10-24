c11e1f2ce44e9d71d75ec7c925f94a74fa1dae03
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-10-24 20:30:22 +0300

    created test for api to get review by id

--- tests/api/student/test_reviews.py
+++ tests/api/student/test_reviews.py
@@ -2,7 +2,7 @@ import allure
 from jsonschema import validate
 
 from SpaceToStudy.api.reviews.client import ReviewsApiClient
-from SpaceToStudy.api.reviews.schemas import SCHEMA_FOR_ALL_REVIEWS
+from SpaceToStudy.api.reviews.schemas import SCHEMA_FOR_ALL_REVIEWS, SCHEMA_FOR_REVIEW_BY_ID
 from tests.api.api_test_runners import APITestRunnerWithStudent
 from tests.utils.value_provider import ValueProvider
 
@@ -16,3 +16,10 @@ class TestAPIReviews(APITestRunnerWithStudent):
         self.assertEqual(200, response.status_code)
         validate(instance=response.json(), schema=SCHEMA_FOR_ALL_REVIEWS)
         self.assertTrue(all(item["rating"] == 5 for item in response.json()["reviews"]))
+
+    def test_get_reviews_by_id(self):
+        client = ReviewsApiClient(ValueProvider.get_base_api_url(), self.accessToken)
+        response = client.get_reviews_by_id("642d3dcac160fddf141253ba")
+        self.assertEqual(200, response.status_code)
+        validate(instance=response.json(), schema=SCHEMA_FOR_REVIEW_BY_ID)
+        self.assertEqual(response.json()["comment"], "cool")
