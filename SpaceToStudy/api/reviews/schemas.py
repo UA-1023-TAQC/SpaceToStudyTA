611fcb0111d010092b1ada53b909351f892aedce
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-10-24 19:34:31 +0300

    added schema for all reviews

---
+++ SpaceToStudy/api/reviews/schemas.py
@@ -0,0 +1,94 @@
+SCHEMA_FOR_ALL_REVIEWS = {
+  "type": "object",
+  "properties": {
+    "count": {
+      "type": "integer"
+    },
+    "reviews": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "_id": {
+            "type": "string"
+          },
+          "comment": {
+            "type": ["string", "null"]
+          },
+          "rating": {
+            "type": "integer"
+          },
+          "author": {
+            "type": ["object", "null"],
+            "properties": {
+              "_id": {
+                "type": "string"
+              },
+              "firstName": {
+                "type": "string"
+              },
+              "lastName": {
+                "type": "string"
+              },
+              "photo": {
+                "type": "string"
+              }
+            }
+          },
+          "targetUserId": {
+            "type": "string"
+          },
+          "targetUserRole": {
+            "type": "string"
+          },
+          "offer": {
+            "type": ["object", "null"],
+            "properties": {
+              "_id": {
+                "type": "string"
+              },
+              "proficiencyLevel": {
+                "type": "array",
+                "items": {
+                  "type": "string"
+                }
+              },
+              "subject": {
+                "type": "object",
+                "properties": {
+                  "_id": {
+                    "type": "string"
+                  },
+                  "name": {
+                    "type": "string"
+                  }
+                }
+              },
+              "category": {
+                "type": "object",
+                "properties": {
+                  "_id": {
+                    "type": "string"
+                  },
+                  "name": {
+                    "type": "string"
+                  }
+                }
+              }
+            }
+          },
+          "createdAt": {
+            "type": "string",
+            "format": "date-time"
+          },
+          "updatedAt": {
+            "type": "string",
+            "format": "date-time"
+          }
+        },
+        "required": ["_id", "rating", "targetUserId", "targetUserRole", "createdAt", "updatedAt"]
+      }
+    }
+  },
+  "required": ["count", "reviews"]
+}
