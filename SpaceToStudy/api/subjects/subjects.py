4e43c7fbfb5470b8e419b6a3532d7d6b809ffb50
Vasul Kichula <vkich7@gmail.com>
2023-10-25 11:03:37 +0300

    some steps further to subjects auth tests

--- SpaceToStudy/api/subjects/subjects.py
+++ SpaceToStudy/api/subjects/subjects.py
@@ -13,21 +13,3 @@ class SubjectsAPIClient(BaseAPIClient):
         response = requests.get(url, headers={"Authorization": f"Bearer {self.access_token}"})
         return response
 
-    def get_subjects_names_by_id(self, category_id):
-        "/ categories / {id} / subjects / names"
-        url = f"{self.url}/{category_id}/subjects/names"
-        response = requests.get(url, headers={"Authorization": f"Bearer {self.access_token}"})
-        return response
-
-    def get_categories_by_id(self, category_id):
-        "/ categories / {id}"
-        url = f"{self.url}/{category_id}"
-        response = requests.get(url, headers={"Authorization": f"Bearer {self.access_token}"})
-        return response
-
-    def get_subjects_by_category_id(self, category_id, name=None):
-        url = f"{self.url}/{category_id}/subjects"
-        if name:
-            url += f"?name={name}"
-        response = requests.get(url, headers={"Authorization": f"Bearer {self.access_token}"})
-        return response
--- tests/api/guest/test_subjects.py
+++ tests/api/guest/test_subjects.py
@@ -1,9 +1,9 @@
 from SpaceToStudy.api.subjects.subjects import SubjectsAPIClient
-from tests.api.api_test_runners import BaseAPITestRunner
+from tests.api.api_test_runners import BaseAPITestRunner, APITestRunnerWithStudent
 from tests.utils.value_provider import ValueProvider
 
 
-class TestAPISubjects(BaseAPITestRunner):
+class TestAPISubjectsUnAthorized(BaseAPITestRunner):
 
     def test_find_all_subjects_unauthorized(self):
         expected_status_code = 401
@@ -14,3 +14,16 @@ class TestAPISubjects(BaseAPITestRunner):
         self.assertEqual(expected_status_code, response.status_code)
         self.assertEqual(expected_code, response.json().get('code'))
         self.assertEqual(expected_message, response.json().get('message'))
+        print(response.status_code)
+        print(response.json().get('code'))
+        print(response.json().get('message'))
+
+class TestAPISubjectsAthorized(APITestRunnerWithStudent):
+
+    def test_find_all_subjects_student(self):
+        client = SubjectsAPIClient(ValueProvider.get_base_api_url(), self.accessToken)
+        response = client.get_subjects()
+        self.assertEqual(response.status_code, 200)
+        print(response.status_code)
+        for i in range(len(response.json().get('items'))):
+            print(response.json().get('items')[i].get('name'))
