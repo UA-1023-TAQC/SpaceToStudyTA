5480041ecf8a9a5c6ff45ea0023918284ca8f075
zakotiukk <zakotiukk@gmail.com>
2023-10-26 20:08:45 +0300

    added methods for api/chat and test_get_messages_in_chat_by_its_id

--- SpaceToStudy/api/chats/client_chats.py
+++ SpaceToStudy/api/chats/client_chats.py
@@ -1,3 +1,4 @@
+import allure
 import requests
 
 from SpaceToStudy.api.base_api_client import BaseAPIClient
@@ -8,7 +9,32 @@ class ChatsAPIClient(BaseAPIClient):
         super().__init__(url, access_token)
         self.url += "chats"
 
+    @allure.step("Post chat")
+    def post_chat(self, chat_data):
+        url = f"{self.url}"
+        response = requests.post(url, json=chat_data, headers={"Authorization": f"Bearer {self.access_token}"})
+        return response
+
+    @allure.step("Get all chats for current user")
     def get_chats(self):
         url = f"{self.url}"
         response = requests.get(url, headers={"Authorization": f"Bearer {self.access_token}"})
+        return response
+
+    @allure.step("Get messages in chat by its {id}")
+    def get_messages_in_chat_by_id(self, chat_id):
+        url = f"{self.url}/{chat_id}/messages"
+        response = requests.get(url, headers={"Authorization": f"Bearer {self.access_token}"})
+        return response
+
+    @allure.step("Delete messages from the chat by its {id}")
+    def delete_messages_from_chat_by_id(self, chat_id):
+        url = f"{self.url}/{chat_id}/messages"
+        response = requests.delete(url, headers={"Authorization": f"Bearer {self.access_token}"})
+        return response
+
+    @allure.step("Delete chat by its {id}")
+    def delete_chat_by_id(self, chat_id):
+        url = f"{self.url}/{chat_id}"
+        response = requests.delete(url, headers={"Authorization": f"Bearer {self.access_token}"})
         return response
--- tests/api/student/test_chats.py
+++ tests/api/student/test_chats.py
@@ -2,16 +2,17 @@ import allure
 from jsonschema import validate
 
 from SpaceToStudy.api.chats.client_chats import ChatsAPIClient
-from SpaceToStudy.api.chats.schemas import SCHEMA_FOR_ALL_CHATS
+from SpaceToStudy.api.chats.schemas import SCHEMA_FOR_ALL_MESSAGES_IN_CHAT
 from tests.api.api_test_runners import APITestRunnerWithStudent
 from tests.utils.value_provider import ValueProvider
 
 
 class TestAPIChats(APITestRunnerWithStudent):
 
-    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/428")
-    def test_get_all_chats(self):
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/458",
+                     "Create test get messages in chat by its {id}")
+    def test_get_messages_in_chat_by_its_id(self):
         client = ChatsAPIClient(ValueProvider.get_base_api_url(), self.accessToken)
-        response = client.get_chats()
+        response = client.get_messages_in_chat_by_id("6532415104a61848c1e6a99b") #KeyError:'id'
         self.assertEqual(response.status_code, 200)
-        validate(instance=response.json(), schema=SCHEMA_FOR_ALL_CHATS)
+        validate(instance=response.json(), schema=SCHEMA_FOR_ALL_MESSAGES_IN_CHAT)
