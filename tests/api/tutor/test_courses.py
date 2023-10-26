2da143a323f6a33ae2e33b834231ffef2f6c9dc4
headles <bobzakon16@gmail.com>
2023-10-26 20:39:52 +0300

    Update

--- tests/api/tutor/test_courses.py
+++ tests/api/tutor/test_courses.py
@@ -1,3 +1,5 @@
+import allure
+
 from SpaceToStudy.api.courses.client_courses import CoursesApiClient
 from SpaceToStudy.api.courses.schemas import SCHEMA_FOR_ALL_COURSES
 from tests.api.api_test_runners import APITestRunnerWithTutor
@@ -6,7 +8,7 @@ from jsonschema import validate
 
 
 class TestAPICourses(APITestRunnerWithTutor):
-
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/474")
     def test_get_courses(self):
         client = CoursesApiClient(ValueProvider.get_base_api_url(), self.accessToken)
         response = client.get_courses()
@@ -14,6 +16,7 @@ class TestAPICourses(APITestRunnerWithTutor):
         self.assertEqual(response.json()["count"], len(response.json()["items"]))
         validate(instance=response.json(), schema=SCHEMA_FOR_ALL_COURSES)
 
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/474")
     def test_get_courses_by_id(self):
         client = CoursesApiClient(ValueProvider.get_base_api_url(), self.accessToken)
         response = client.get_courses_by_id()  # There should be an ID here when it appears
