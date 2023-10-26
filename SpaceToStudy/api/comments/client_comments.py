560cd0d1f1a50d1a65992f510038a69c4a135425
headles <bobzakon16@gmail.com>
2023-10-26 15:49:31 +0300

    Update

--- SpaceToStudy/api/comments/client_comments.py
+++ SpaceToStudy/api/comments/client_comments.py
@@ -1,3 +1,4 @@
+import allure
 import requests
 
 from SpaceToStudy.api.base_api_client import BaseAPIClient
@@ -8,7 +9,13 @@ class CommentsAPIClient(BaseAPIClient):
         super().__init__(url, access_token)
         self.url += "cooperations"
 
+    @allure.step("Receiving cooperation comments")
     def get_comments(self, cooperation_id):
+        """
+        Gets comments to collaborate with the specified ID.
+        :param cooperation_id: ID of the collaboration for which you want to get comments.
+        :return: A response object from the server with a list of comments.
+        """
         url = f"{self.url}/{cooperation_id}/comments"
         response = requests.get(url, headers={"Authorization": f"Bearer {self.access_token}"})
         return response
--- tests/api/student/test_comments.py
+++ tests/api/student/test_comments.py
@@ -1,3 +1,5 @@
+import allure
+
 from SpaceToStudy.api.comments.client_comments import CommentsAPIClient
 from SpaceToStudy.api.comments.schemas import SCHEMA_FOR_COMMENTS
 from tests.api.api_test_runners import APITestRunnerWithStudent
@@ -6,7 +8,7 @@ from jsonschema import validate
 
 
 class TestAPIComments(APITestRunnerWithStudent):
-
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/461")
     def test_get_comments(self):
         client = CommentsAPIClient(ValueProvider.get_base_api_url(), self.accessToken)
         response = client.get_comments("65097f598f5d654793fed7ef")
