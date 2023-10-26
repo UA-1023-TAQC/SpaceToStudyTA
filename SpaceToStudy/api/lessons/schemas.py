52ab4a24b8c00212a062e942d1bc56b17fc3442b
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-10-26 18:30:53 +0300

    created schemas for lessons

---
+++ SpaceToStudy/api/lessons/schemas.py
@@ -0,0 +1,68 @@
+SCHEMA_FOR_ALL_LESSONS = {
+    "type": "object",
+    "properties": {
+        "count": {
+            "type": "integer",
+            "minimum": 0
+        },
+        "items": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "category": {"type": ["object", "null"]},
+                    "_id": {"type": "string"},
+                    "author": {"type": "string"},
+                    "title": {"type": "string"},
+                    "description": {"type": "string"},
+                    "attachments": {"type": "array"},
+                    "createdAt": {"type": "string", "format": "date-time"},
+                    "updatedAt": {"type": "string", "format": "date-time"}
+                },
+                "required": ["_id", "author", "title", "description", "attachments", "createdAt", "updatedAt"]
+            }
+        }
+    },
+    "required": ["count", "items"]
+}
+
+SCHEMA_FOR_POST_LESSON = {
+    "type": "object",
+    "properties": {
+        "title": {"type": "string"},
+        "description": {"type": "string"},
+        "content": {"type": "string"},
+        "author": {"type": "string"},
+        "attachments": {
+            "type": "array",
+            "items": {"type": "string"}
+        },
+        "category": {
+            "type": "object",
+            "properties": {
+                "_id": {"type": "string"},
+                "name": {"type": "string"}
+            },
+            "required": ["_id", "name"]
+        },
+        "_id": {"type": "string"},
+        "createdAt": {"type": "string", "format": "date-time"},
+        "updatedAt": {"type": "string", "format": "date-time"}
+    },
+    "required": ["title", "description", "content", "author", "attachments", "category", "_id", "createdAt",
+                 "updatedAt"]
+}
+
+SCHEMA_FOR_LESSON_BY_ID = {
+    "type": "object",
+    "properties": {
+        "_id": {"type": "string"},
+        "author": {"type": "string"},
+        "title": {"type": "string"},
+        "description": {"type": "string"},
+        "attachments": {"type": "array"},
+        "createdAt": {"type": "string", "format": "date-time"},
+        "updatedAt": {"type": "string", "format": "date-time"}
+    },
+    "required": ["_id", "author", "title", "description", "attachments", "createdAt", "updatedAt"]
+}
