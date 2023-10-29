7b64612c0ee9e1c8c1ca493013f0934191e07756
Natali-natali <rozdilska.n@gmail.com>
2023-10-29 14:35:56 +0200

    update

--- SpaceToStudy/api/cooperations/schemas.py
+++ SpaceToStudy/api/cooperations/schemas.py
@@ -169,85 +169,168 @@ ALL_COOPERATIONS_SCHEMA = {
 }
 
 SCHEMA_COOPERATIONS_ID = {
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "title": "Generated schema for Root",
-  "type": "object",
-  "properties": {
-    "_id": {
-      "type": "string"
-    },
-    "offer": {
-      "type": "object",
-      "properties": {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "title": "Generated schema for Root",
+    "type": "object",
+    "properties": {
         "_id": {
-          "type": "string"
+            "type": "string"
         },
         "price": {
-          "type": "number"
+            "type": "number"
+        },
+        "proficiencyLevel": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
+        },
+        "title": {
+            "type": "string"
+        },
+        "description": {
+            "type": "string"
+        },
+        "languages": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
+        },
+        "authorRole": {
+            "type": "string"
         },
         "author": {
-          "type": "string"
-        }
-      },
-      "required": [
+            "type": "object",
+            "properties": {
+                "_id": {
+                    "type": "string"
+                },
+                "firstName": {
+                    "type": "string"
+                },
+                "lastName": {
+                    "type": "string"
+                },
+                "totalReviews": {
+                    "type": "object",
+                    "properties": {
+                        "student": {
+                            "type": "number"
+                        },
+                        "tutor": {
+                            "type": "number"
+                        }
+                    },
+                    "required": [
+                        "student",
+                        "tutor"
+                    ]
+                },
+                "averageRating": {
+                    "type": "object",
+                    "properties": {
+                        "student": {
+                            "type": "number"
+                        },
+                        "tutor": {
+                            "type": "number"
+                        }
+                    },
+                    "required": [
+                        "student",
+                        "tutor"
+                    ]
+                },
+                "photo": {
+                    "type": "string"
+                },
+                "professionalSummary": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "_id",
+                "firstName",
+                "lastName",
+                "totalReviews",
+                "averageRating",
+                "photo",
+                "professionalSummary"
+            ]
+        },
+        "subject": {
+            "type": "object",
+            "properties": {
+                "_id": {
+                    "type": "string"
+                },
+                "name": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "_id",
+                "name"
+            ]
+        },
+        "category": {
+            "type": "object",
+            "properties": {
+                "_id": {
+                    "type": "string"
+                },
+                "appearance": {
+                    "type": "object",
+                    "properties": {
+                        "icon": {
+                            "type": "string"
+                        },
+                        "color": {
+                            "type": "string"
+                        }
+                    },
+                    "required": [
+                        "icon",
+                        "color"
+                    ]
+                }
+            },
+            "required": [
+                "_id",
+                "appearance"
+            ]
+        },
+        "status": {
+            "type": "string"
+        },
+        "FAQ": {
+            "type": "array",
+            "items": {}
+        },
+        "createdAt": {
+            "type": "string"
+        },
+        "updatedAt": {
+            "type": "string"
+        },
+        "chatId": {}
+    },
+    "required": [
         "_id",
         "price",
-        "author"
-      ]
-    },
-    "initiator": {
-      "type": "string"
-    },
-    "initiatorRole": {
-      "type": "string"
-    },
-    "receiver": {
-      "type": "string"
-    },
-    "receiverRole": {
-      "type": "string"
-    },
-    "proficiencyLevel": {
-      "type": "string"
-    },
-    "price": {
-      "type": "number"
-    },
-    "status": {
-      "type": "string"
-    },
-    "needAction": {
-      "type": "string"
-    },
-    "availableQuizzes": {
-      "type": "array",
-      "items": {}
-    },
-    "finishedQuizzes": {
-      "type": "array",
-      "items": {}
-    },
-    "createdAt": {
-      "type": "string"
-    },
-    "updatedAt": {
-      "type": "string"
-    }
-  },
-  "required": [
-    "_id",
-    "offer",
-    "initiator",
-    "initiatorRole",
-    "receiver",
-    "receiverRole",
-    "proficiencyLevel",
-    "price",
-    "status",
-    "needAction",
-    "availableQuizzes",
-    "finishedQuizzes",
-    "createdAt",
-    "updatedAt"
-  ]
+        "proficiencyLevel",
+        "title",
+        "description",
+        "languages",
+        "authorRole",
+        "author",
+        "subject",
+        "category",
+        "status",
+        "FAQ",
+        "createdAt",
+        "updatedAt",
+        "chatId"
+    ]
 }
