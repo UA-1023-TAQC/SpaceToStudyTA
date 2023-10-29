40af47e57511de7aa25d7a2a22da8098a26ac530
Natali-natali <rozdilska.n@gmail.com>
2023-10-29 14:29:54 +0200

    test_find_offers_by_id_with_correct_category_and_subject_id

--- tests/api/student/test_offers.py
+++ tests/api/student/test_offers.py
@@ -1,5 +1,6 @@
 import allure
 from jsonschema import validate
+from parameterized import parameterized
 
 from SpaceToStudy.api.offers.client_offers import OffersApiClient
 from SpaceToStudy.api.offers.schemas import ALL_OFFERS_SCHEMA, SCHEMA_OFFERS_ID
@@ -32,3 +33,18 @@ class TestOffersApi(APITestRunnerWithStudent):
         response = client.get_offers_by_id("652ea7336fc04ef55bb462cf")
         self.assertEqual(200, response.status_code)
         validate(instance=response.json(), schema=SCHEMA_OFFERS_ID)
+
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/491#issue-1966961096")
+    @parameterized.expand([
+        ("64944a54e874088383fdfb84", "64884f33fdc2d1a130c24ac2", "64885108fdc2d1a130c24af9"),
+        ("64ac054c137130e45017f841", "64884f21fdc2d1a130c24ac0", "648850c4fdc2d1a130c24aea"),
+    ])
+    def test_find_offers_by_id_with_correct_category_and_subject_id(self, offer_id, category_id, subject_id):
+        client = OffersApiClient(ValueProvider.get_base_api_url(), self.accessToken)
+        response = client.get_offers_by_id(offer_id)
+        category_id_of_offer = response.json()["category"]["_id"]
+        subject_id_of_offer = response.json()["subject"]["_id"]
+        self.assertEqual(200, response.status_code)
+        validate(instance=response.json(), schema=SCHEMA_OFFERS_ID)
+        self.assertEqual(category_id, category_id_of_offer)
+        self.assertEqual(subject_id, subject_id_of_offer)
