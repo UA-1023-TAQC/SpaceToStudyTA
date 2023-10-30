adbd476d3f0b757b1b27a378ae110f1cbf590503
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-10-30 17:51:16 +0200

    created negative test for api to post subject with unauthorized user

--- tests/api/guest/test_subjects.py
+++ tests/api/guest/test_subjects.py
@@ -30,3 +30,14 @@ class TestAPISubjects(BaseAPITestRunner):
         self.assertEqual(401, response.status_code)
         self.assertEqual(expected_response, response.json())
 
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/498")
+    def test_post_subject_unauthorized(self):
+        client = SubjectsApiClient(ValueProvider.get_base_api_url())
+        response = client.post_subjects({"name": "Klingon", "category": "64884fedfdc2d1a130c24ade"})
+        expected_response = {
+          "status": 401,
+          "code": "UNAUTHORIZED",
+          "message": "The requested URL requires user authorization."
+        }
+        self.assertEqual(401, response.status_code)
+        self.assertEqual(expected_response, response.json())
