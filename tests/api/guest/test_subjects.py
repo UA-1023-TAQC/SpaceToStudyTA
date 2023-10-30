49f80f734298f45d0d68cae60af495526c88056c
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-10-30 18:01:09 +0200

    created negative test for api to delete subject by unauthorized user

--- tests/api/guest/test_subjects.py
+++ tests/api/guest/test_subjects.py
@@ -39,3 +39,10 @@ class TestAPISubjects(BaseAPITestRunner):
         response = client.patch_subject_by_id("64885121fdc2d1a130c24afc", {"name": "Klingon"})
         self.assertEqual(401, response.status_code)
         self.assertEqual(UNAUTHORIZED_RESPONSE, response.json())
+
+    @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/500")
+    def test_delete_subject_unauthorized(self):
+        client = SubjectsApiClient(ValueProvider.get_base_api_url())
+        response = client.delete_subject("64885121fdc2d1a130c24afc")
+        self.assertEqual(401, response.status_code)
+        self.assertEqual(UNAUTHORIZED_RESPONSE, response.json())
