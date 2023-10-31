418d44986aa13ebe9f21b117f404303c4d2af428
Vasul Kichula <vkich7@gmail.com>
2023-10-31 11:11:47 +0200

    Added test res-categories (unathorized)

--- SpaceToStudy/api/categories/client.py
+++ SpaceToStudy/api/categories/client.py
@@ -2,7 +2,6 @@ import requests
 
 from SpaceToStudy.api.base_api_client import BaseAPIClient
 
-
 class CategoriesApiClient(BaseAPIClient):
     def __init__(self, url, access_token=None):
         super().__init__(url, access_token)
