7ac1253a956be174355f5f63f113fd4c973a9e2b
Illia Nakonechnyi <realpeople2807@gmail.com>
2023-11-07 12:08:34 +0200

    added Scenario Verify that the user cannot close the login modal by clicking outside of it

--- features/login_modal.feature
+++ features/login_modal.feature
@@ -5,6 +5,6 @@ Feature: Verify login modal
 
   Scenario: Verify that the user cannot close the login modal by clicking outside of it
     When I click the "Login" button in the upper right corner of the header
-    When A modal window with the title "Welcome" opens
-    When I сlick outside the login modal
+    Then A modal window with the title "Welcome" opens
+    When I click outside the login modal
     Then Login modal remained open
--- features/steps/login_modal_steps.py
+++ features/steps/login_modal_steps.py
@@ -1,2 +1,22 @@
 from behave import *
 
+from SpaceToStudy.ui.pages.home_page.home_guest import HomePageGuest
+from SpaceToStudy.ui.pages.login_modal.login_modal import LoginModal
+
+
+@when('I click the "Login" button in the upper right corner of the header')
+def click_login_btn(context):
+    (HomePageGuest(context.driver)
+     .get_header()
+     .click_login_btn())
+
+
+@then('A modal window with the title "Welcome" opens')
+@then('Login modal remained open')
+def open_welcome_login_modal(context):
+    assert LoginModal(context).get_title_text() == "Welcome back"
+
+
+@when('I click outside the login modal')
+def click_outside_the_login_modal(context):
+    LoginModal(context).outside_click()
