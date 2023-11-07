ad7810f10371105d2645ec7a91138b81a025287f
zakotiukk <zakotiukk@gmail.com>
2023-11-07 13:18:55 +0200

    changed path for SCHEMA_FOR_ERRORS

--- tests/api/student/test_chats.py
+++ tests/api/student/test_chats.py
@@ -2,7 +2,8 @@ import allure
 from jsonschema import validate
 
 from SpaceToStudy.api.chats.client_chats import ChatsAPIClient
-from SpaceToStudy.api.chats.schemas import SCHEMA_FOR_ALL_MESSAGES_IN_CHAT, ERROR_SCHEMA
+from SpaceToStudy.api.chats.schemas import SCHEMA_FOR_ALL_MESSAGES_IN_CHAT
+from SpaceToStudy.api.schema_for_errors import SCHEMA_FOR_ERRORS
 from tests.api.api_test_runners import APITestRunnerWithStudent
 from tests.utils.value_provider import ValueProvider
 
@@ -23,7 +24,7 @@ class TestAPIChats(APITestRunnerWithStudent):
         client = ChatsAPIClient(ValueProvider.get_base_api_url(), self.accessToken)
         response = client.get_messages_in_chat_by_id("650df5a9c5694d1d9d4643cd")
         self.assertEqual(response.status_code, 403)
-        validate(instance=response.json(), schema=ERROR_SCHEMA)
+        validate(instance=response.json(), schema=SCHEMA_FOR_ERRORS)
 
     @allure.testcase("https://github.com/UA-1023-TAQC/SpaceToStudyTA/issues/520",
                      "Create a negative test get messages from chat by its id (404:NOT FOUND)")
@@ -31,4 +32,4 @@ class TestAPIChats(APITestRunnerWithStudent):
         client = ChatsAPIClient(ValueProvider.get_base_api_url(), self.accessToken)
         response = client.get_messages_in_chat_by_id("650df5a9c5694d1d9d46439b")
         self.assertEqual(response.status_code, 404)
-        validate(instance=response.json(), schema=ERROR_SCHEMA)
+        validate(instance=response.json(), schema=SCHEMA_FOR_ERRORS)
